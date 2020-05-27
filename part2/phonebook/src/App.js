5392f32a3ca8b8dabde4e5d4de34280365cc2a77
eedenong <e0325639@u.nus.edu>
2020-05-27 13:00:46 +0800

    Ex 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas',
-      number: 92929999 }
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setFilterValue] = useState('')
+  const [ showAll, setShowAll ] = useState(true)
 
   // event handler for add button
   const addPerson = (event) => {
@@ -42,6 +47,11 @@ const App = () => {
     
   }
 
+  // event handler for filter button
+  const showFiltered = (event) => {
+    event.preventDefault()
+    setShowAll(!showAll)
+  }
   // event handler for name input
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -52,9 +62,28 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  // event handler for filter input
+  const handleFilterChange = (event) => {
+    setFilterValue(event.target.value)
+  }
+
+  const personsToShow = showAll ? persons : persons.filter(person => {
+    const nameLowerCased = person.name.toLowerCase()
+    const filterLowerCased = newFilter.toLowerCase()
+    console.log('name lower cased is', nameLowerCased);
+    console.log('filter name lower cased is', filterLowerCased);
+    return nameLowerCased.includes(filterLowerCased)
+  })
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={showFiltered} >
+        <div>
+          filter shown with: <input value={newFilter} onChange={handleFilterChange} />
+        </div>
+      </form>
+      <h2>Add new Number</h2>
       <form onSubmit={addPerson} >
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -68,7 +97,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+        {personsToShow.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
       </div>
     </div>
   )
