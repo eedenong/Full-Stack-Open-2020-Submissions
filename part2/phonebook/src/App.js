ac75dff6e7a729163b79940ee32e9a6dde0c7c98
eedenong <e0325639@u.nus.edu>
2020-05-27 12:37:43 +0800

    Ex 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: 92929999 }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   // event handler for add button
   const addPerson = (event) => {
@@ -19,20 +21,42 @@ const App = () => {
 
     if (dupeExists) {
       alert(`${newName} is already added to phonebook`)
-    } else {
+    }
+
+    // check if the number entered is valid
+    const invalidNumber = isNaN(newNumber)
+
+    if (invalidNumber) {
+      alert(`${newNumber} is not a valid number!`)
+    }
+
+    const validEntry = !dupeExists && !invalidNumber
+
+    if (validEntry) {
+      console.log('entry is valid, createing person');
       // create a person and add it
       const newPerson = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setNewNumber('')
+    } else {
+      console.log('entry is invalid');
     }
+      
     
   }
 
   // event handler for name input
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  // event handler for number input
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -43,12 +67,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <div key={person.name}>{person.name}</div>)}
+        {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
       </div>
     </div>
   )
