25bc536900a85970d0bd06af4fdc0a1f0d9ad841
eedenong <e0325639@u.nus.edu>
2020-06-04 01:45:21 +0800

    Ex 3.22

--- part2/phonebook/.eslintrc.js
+++ part2/phonebook/.eslintrc.js
@@ -1,33 +1,44 @@
 module.exports = {
     'env': {
-        'browser': true,
-        'commonjs': true,
-        'es6': true
+      'browser': true,
+      'commonjs': true,
+      'es6': true,
+      'node': true
     },
     'extends': 'eslint:recommended',
     'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
+      'Atomics': 'readonly',
+      'SharedArrayBuffer': 'readonly'
     },
     'parserOptions': {
-        'ecmaVersion': 11
+      'ecmaVersion': 11,
+      'sourceType': "module"
     },
     'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
+      'indent': [
+        'error',
+        2
+      ],
+      'linebreak-style': [
+        'error',
+        'unix'
+      ],
+      'quotes': [
+        'error',
+        'single'
+      ],
+      'semi': [
+        'error',
+        'never'
+      ],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error', 'always'
+      ],
+      'arrow-spacing': [
+        'error', { 'before': true, 'after': true }
+      ],
+      'no-console': 0
     }
-}
+  }
