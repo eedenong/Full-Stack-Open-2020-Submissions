6cc36476ed093a825bc0465a5070556458f538e4
eedenong <e0325639@u.nus.edu>
2020-06-23 15:56:47 +0800

    Ex 5.1, 5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -18,13 +18,14 @@ const App = () => {
 
   //check if user details can be found in local storage
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedBlogappuser')
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    console.log('loggedUserJSON is', loggedUserJSON)
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
       blogService.setToken(user.token)
     }
-  })
+  }, [])
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -32,9 +33,14 @@ const App = () => {
       // login and get the response data
       const user = await loginService.login({ username, password })
       //set the token
-      loginService.setToken(user.token)
+      blogService.setToken(user.token)
       //save the user to local storage
       window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+      console.log('loggedUserJSON', loggedUserJSON)
+      if (loggedUserJSON) {
+        console.log('user successfully logged in json local storage', loggedUserJSON)
+      }
       //set user
       setUser(user)
       //reset username and password fields
@@ -49,12 +55,30 @@ const App = () => {
 
   }
 
+  const handleLogout = (event) => {
+    event.preventDefault()
+    try {
+      //remove the token
+      blogService.setToken(null)
+      //remove the user from local storage
+      window.localStorage.removeItem('loggedBlogappUser')
+      //set the user state to null
+      setUser(null)
+    } catch (exception) {
+      console.log(exception)
+    }
+  }
+
   const showBlogs = () => (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <div>{user.name} logged in <button onClick={handleLogout}>logout</button> </div>
+      <br />
+      <div>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
     </div>
   )
 
@@ -80,6 +104,7 @@ const App = () => {
             onChange={({ target }) => setPassword(target.value)}
           />
         </div>
+        <button type="submit">login</button>
       </form>
     </div>
   )
